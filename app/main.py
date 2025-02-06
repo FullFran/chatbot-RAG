9bd769360f9856585aeaec3f3e8bd09879c9611c
Francisco Manuel Olmedo Cortés <128989182+FullFran@users.noreply.github.com>
2025-02-06 02:38:30 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
